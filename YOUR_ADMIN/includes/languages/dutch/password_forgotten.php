1608e35771a09e2b0bad8122bf8a0bec3a2f6ce8
Erik <info@zen4all.nl>
2012-11-18 10:57:18 +0100

    MESSAGE_PASSWORD_SEND aangepast

--- YOUR_ADMIN/includes/languages/dutch/password_forgotten.php
+++ YOUR_ADMIN/includes/languages/dutch/password_forgotten.php
@@ -17,8 +17,7 @@ define('TEXT_BUTTON_CANCEL', 'annuleren');
 
 define('ERROR_WRONG_EMAIL', 'U heeft het verkeerde e-mailadres ingevuld.');
 define('ERROR_WRONG_EMAIL_NULL', 'Jammer maar helaas, leuk geprobeerd :-P');
-define('MESSAGE_PASSWORD_SENT', 'Het nieuwe wachtwoord is verzonden naar het opgegeven e-mailadres.<br />Klik hieronder op "inloggen" om in te loggen met het nieuwe tijdelijke wachtwoord');
-
+define('MESSAGE_PASSWORD_SENT', 'Bedankt. Als het door u ingevoerde e-mailadres overeenkomt met een admin account in onze database, dan wordt er een ieuw wachtwoord gestuurd naar dat adres.<br />Lees alstublieft de e-mail en klik dan hieronder op "inloggen" om in te loggen met het nieuwe tijdelijke wachtwoord.');
 define('TEXT_EMAIL_SUBJECT_PWD_RESET', 'Uw aanvraag voor wijziging admin wachtwoord ');
 define('TEXT_EMAIL_MESSAGE_PWD_RESET', 'Een nieuw wachtwoord werd gevraagd door %s.' . "\n\n" . 'Uw nieuwe tijdelijke wachtwoord is:' . "\n\n   %s\n\nU wordt gevraagd om een nieuw wachtwoord te kiezen voordat u kunt inloggen.\n\nDit tijdelijke wachtwoord verloopt na 24 uur.\n\n\n");
 
