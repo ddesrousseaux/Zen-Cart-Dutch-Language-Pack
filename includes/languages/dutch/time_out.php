ec4fe616b15b273cde5b465200a85ed40c78143b
Erik <info@zen4all.nl>
2012-11-19 21:26:27 +0100

    regel 15 vertaald

--- includes/languages/dutch/time_out.php
+++ includes/languages/dutch/time_out.php
@@ -12,7 +12,7 @@
 define('NAVBAR_TITLE', 'Maximale inlogtijd overschreden');
 define('HEADING_TITLE', 'Sorry! Uw inlogsessie is niet langer geldig.');
 define('HEADING_TITLE_LOGGED_IN', 'Sorry! Deze actie is afgeschermd. ');
-define('TEXT_INFORMATION', '<p>Als u een bestelling aan het plaatsen was log dan in en uw winkelwagen zal worden hersteld. U kunt daarna opnieuw afrekenen.</p><p>Als u een bestelling had geplaatst en u de status wilde controleren' . (DOWNLOAD_ENABLED == 'true' ? ', of als het om een artikel gaat dat u wilde downloaden' : '') . ', Ga dan naar de <a href="' . zen_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">Mijn account</a> pagina om uw bestelling terug te zien.</p><p>If you are using a <b>CheckOut WithOut an Account</b> connection, your session was timed out because you connected via an Order Status E-Mail and you must start over.</p>');
+define('TEXT_INFORMATION', '<p>Als u een bestelling aan het plaatsen was log dan in en uw winkelwagen zal worden hersteld. U kunt daarna opnieuw afrekenen.</p><p>Als u een bestelling had geplaatst en u de status wilde controleren' . (DOWNLOAD_ENABLED == 'true' ? ', of als het om een artikel gaat dat u wilde downloaden' : '') . ', Ga dan naar de <a href="' . zen_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">Mijn account</a> pagina om uw bestelling terug te zien.</p><p>Als u een <b>CheckOut zonder een Account</b> verbinding heeft, dan is uw sessie verlopen, omdat u verbonden was via een Orderstatus E-mail en moet u overnieuw beginnen.</p>');
 
 define('TEXT_INFORMATION_LOGGED_IN', 'U bent ingelogd en kan als u dat wil verder winkelen. Kies een categorie uit het menu.');
 
